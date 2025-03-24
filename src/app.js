f07e9182830efaac100b1d518a4913df3a11af4c
T1t0-G40n4 <titogaona84@gmail.com>
2025-03-24 19:26:23 +0000

    Generador de Excusas Tito Gaona

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function excuseGenerator(){
+
+  let whoRandom = Math.floor(Math.random()* who.length);
+  let actionRandom = Math.floor(Math.random() * action.length);
+  let whatRandom = Math.floor(Math.random() * what.length);
+  let whenRandom = Math.floor(Math.random() * when.length);
+
+  let excuse = who[whoRandom] + ' ' + action[actionRandom] + ' ' + what[whatRandom] + ' ' + when[whenRandom];
+
+  document.getElementById("excuse").innerHTML = excuse;
+  
+}
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+excuseGenerator();
+
+
+}
